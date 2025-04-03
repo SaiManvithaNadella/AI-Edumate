d47fb9b3a329e4bb63a57547a110f9f6b42a1e74
SaiManvithaNadella <manvithanadella12345@gmail.com>
2025-04-03 18:59:38 -0400

    added render host

--- ai-edumate-frontend/src/api/index.js
+++ ai-edumate-frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
+const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://0.0.0.0:10000';
 
 const api = axios.create({
   baseURL,
